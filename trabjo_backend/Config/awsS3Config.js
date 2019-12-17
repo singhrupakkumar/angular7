88b29b1b940dc73b28914a43981e75b1d8af43ac
Rupak <rupak@avainfotech.com>
2019-12-17 17:45:12 +0530

    dddd

--- trabjo_backend/Config/awsS3Config.js
+++ trabjo_backend/Config/awsS3Config.js
@@ -1,9 +1,9 @@
 'use strict';
 let s3BucketCredentials = {
-    "bucket": "haytrabajoya",
-    "accessKeyId": "AKIAT33RO3NB42APL747",
-    "secretAccessKey": "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",
-    "s3URL": "https://s3-us-west-2.amazonaws.com/haytrabajoya/"
+    "bucket": "fdgdfg",
+    "accessKeyId": "fdgdg",
+    "secretAccessKey": "fdgd/dfg/",
+    "s3URL": "fdgd"
 };
 
 module.exports = {
